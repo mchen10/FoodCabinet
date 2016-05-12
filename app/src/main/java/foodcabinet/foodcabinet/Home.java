481e17053beb4f2d11ec876d8eb2265e26e2a4cd
sahajp23 <Sahaj@Venkatas-MacBook-Pro.local>
2016-05-12 16:11:13 -0700

    Basic Home Page

---
+++ app/src/main/java/foodcabinet/foodcabinet/Home.java
@@ -0,0 +1,40 @@
+package foodcabinet.foodcabinet;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+/**
+ * Created by Sahaj on 5/12/16.
+ */
+public class Home extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.app_bar);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    /**
+     * Method called when the user selects the button to take a picture of an item
+     */
+    public void takePicture()
+    {
+
+    }
+
+
+}
