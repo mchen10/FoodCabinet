e744df78d66eac17663b1c0445dc8b4cb0cf1e52
jyang58 <jeff688y@gmail.com>
2016-05-15 16:48:05 -0700

    Predictors
    Not yet done

---
+++ app/src/main/java/foodcabinet/foodcabinet/ExpirationDatePredictor.java
@@ -0,0 +1,10 @@
+package foodcabinet.foodcabinet;
+
+/**
+ * Created by Jeff on 5/14/2016.
+ */
+public class ExpirationDatePredictor {
+    public ExpirationDatePredictor(Product product){
+
+    }
+}
---
+++ app/src/main/java/foodcabinet/foodcabinet/UsedDatePredictor.java
@@ -0,0 +1,10 @@
+package foodcabinet.foodcabinet;
+
+/**
+ * Created by Jeff on 5/14/2016.
+ */
+public class UsedDatePredictor {
+    public UsedDatePredictor(Product product){
+
+    }
+}
