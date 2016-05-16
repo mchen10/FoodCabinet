944504ea4d3c777c46d8db46ad4ecca1de4b9fcc
sahajp23 <Sahaj@Venkatas-MacBook-Pro.local>
2016-05-15 22:36:01 -0700

    Basic Updates

--- app/src/main/java/foodcabinet/foodcabinet/PictureToText.java
+++ app/src/main/java/foodcabinet/foodcabinet/PictureToText.java
@@ -1,28 +1,51 @@
 package foodcabinet.foodcabinet;
-import android.media.Image;
-import java.io.File;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.vision.v1.Vision;
+import com.google.api.services.vision.v1.VisionRequest;
+import com.google.api.services.vision.v1.VisionScopes;
+import com.google.api.services.vision.v1.model.Image;
+import com.google.api.client.json.JsonFactory;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 /**
- * Created by Sahaj on 5/14/16.
+ * Created by Sahaj on 5/15/16.
  */
 public class PictureToText {
 
-    Image pic;
-    String translate;
+        private String transated;
+        private Image picture;
+        private Product prod;
+        public static Vision connectToOCR() throws IOException,GeneralSecurityException
+        {
+                GoogleCredential credential =
+                        GoogleCredential.getApplicationDefault().createScoped(VisionScopes.all());
+                JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+                return new Vision.Builder(GoogleNetHttpTransport.newTrustedTransport(), jsonFactory, credential)
+                        .setApplicationName(APPLICATION_NAME)
+                        .build();
 
-    public PictureToText(Image p)
-    {
+        }
+
+
+        public PictureToText(Image a)
+        {
+            picture=a;
+
+        }
+
+        public void TextToProduct()
+        {
+
+
+
+        }
+
+
+
 
     }
-
-    public void translateImage()
-    {
-
-
-    }
-
-    public String getTranslate()
-    {
-        return translate;
-    }
-
 }
