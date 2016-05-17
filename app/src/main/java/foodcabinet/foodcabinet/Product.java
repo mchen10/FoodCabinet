dbfdef01218955a9b9904f196fcc63998bed5a0b
jyang58 <jeff688y@gmail.com>
2016-05-17 00:50:19 -0700

    kinda changing Product
    someone fix this class plz so that it returns a number of days for uDate
    and eDate instead of strings

--- app/src/main/java/foodcabinet/foodcabinet/Product.java
+++ app/src/main/java/foodcabinet/foodcabinet/Product.java
@@ -2,16 +2,21 @@ package foodcabinet.foodcabinet;
 
 import android.media.Image;
 
+import java.sql.Time;
+import java.util.Date;
+
 /**
  * Created by Sahaj on 5/14/16.
  */
 public class Product {
     private String name, eDate, uDate;
+    private Date bDate;
     private Image pic;
     public Product(String name, String expirationDate, String usedDate) {
         this.name = name;
         eDate = expirationDate;
         uDate = usedDate;
+        bDate = new Date();
     }
     public String getName() {
         return name;
@@ -21,6 +26,9 @@ public class Product {
     }
     public String getUDate() {
         return uDate;
+    }
+    public Date getBDate(){
+        return bDate;
     }
     public Image getPic() {
         return pic;
