a5cdd94071a2dfb053e1ae770b90afb5e256dc61
jyang58 <jeff688y@gmail.com>
2016-05-17 00:55:14 -0700

    Revert "kinda changing Product"
    This reverts commit dbfdef01218955a9b9904f196fcc63998bed5a0b.

--- app/src/main/java/foodcabinet/foodcabinet/Product.java
+++ app/src/main/java/foodcabinet/foodcabinet/Product.java
@@ -2,21 +2,16 @@ package foodcabinet.foodcabinet;
 
 import android.media.Image;
 
-import java.sql.Time;
-import java.util.Date;
-
 /**
  * Created by Sahaj on 5/14/16.
  */
 public class Product {
     private String name, eDate, uDate;
-    private Date bDate;
     private Image pic;
     public Product(String name, String expirationDate, String usedDate) {
         this.name = name;
         eDate = expirationDate;
         uDate = usedDate;
-        bDate = new Date();
     }
     public String getName() {
         return name;
@@ -26,9 +21,6 @@ public class Product {
     }
     public String getUDate() {
         return uDate;
-    }
-    public Date getBDate(){
-        return bDate;
     }
     public Image getPic() {
         return pic;
