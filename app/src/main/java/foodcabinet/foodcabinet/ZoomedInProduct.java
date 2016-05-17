521345613a124ece88da1fc58ebbfcd28fe4b489
Instatutors <chen.michael9@gmail.com>
2016-05-17 07:34:29 -0700

    Clicking leads to zoom in of product

--- app/src/main/java/foodcabinet/foodcabinet/ZoomedInProduct.java
+++ app/src/main/java/foodcabinet/foodcabinet/ZoomedInProduct.java
@@ -1,7 +1,14 @@
 package foodcabinet.foodcabinet;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by Mems on 5/16/2016.
@@ -12,5 +19,17 @@ public class ZoomedInProduct extends AppCompatActivity{
         setContentView(R.layout.zoomed_in_product);
 
         Product p = (Product) getIntent().getSerializableExtra("Product");
+        TextView name = (TextView) findViewById(R.id.NameZoomed);
+        ImageView image = (ImageView) findViewById(R.id.ImageZoomed);
+        TextView usedDate = (TextView) findViewById(R.id.UsedDateZoomed);
+        TextView expirDate = (TextView) findViewById(R.id.ExpirationDateZoomed);
+
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+    }
+
+    public void returnHome(View v) {
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
     }
 }
