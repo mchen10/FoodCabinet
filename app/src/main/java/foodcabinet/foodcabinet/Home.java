ae8acf25e71b844dab52b92d9a363e70a8d01ceb
Instatutors <chen.michael9@gmail.com>
2016-05-16 19:33:54 -0700

    Brief UI update

--- app/src/main/java/foodcabinet/foodcabinet/Home.java
+++ app/src/main/java/foodcabinet/foodcabinet/Home.java
@@ -51,7 +51,11 @@ public class Home extends AppCompatActivity{
         products.add(new Product("Bread", "August 25, 2015", "August 20, 2015"));
         products.add(new Product("Bread", "August 25, 2015", "August 20, 2015"));
         LinearLayout.LayoutParams picLp = new LinearLayout.LayoutParams((screenWidth - 30) / 3, screenWidth / 3);
+        LinearLayout.LayoutParams textLp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        textLp.setMargins(0, 0, 0, 10);
         LinearLayout.LayoutParams layoutLp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        LinearLayout.LayoutParams prodLp = new LinearLayout.LayoutParams((screenWidth - 20) / 3, 100);
+        prodLp.setMargins(0, 0, 0, 20);
         for (int i = 0; i < products.size(); i+=3) {
             LinearLayout layout = new LinearLayout(this);
             layout.setOrientation(LinearLayout.HORIZONTAL);
@@ -59,6 +63,13 @@ public class Home extends AppCompatActivity{
             layout.setLayoutParams(layoutLp);
             if (i + 1 == products.size()) {
                 LinearLayout b1 = new LinearLayout(this);
+                b1.setClickable(true);
+                b1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                });
                 b1.setOrientation(LinearLayout.VERTICAL);
 
                 ImageView pic1 = new ImageView(this);
@@ -69,19 +80,20 @@ public class Home extends AppCompatActivity{
 
                 TextView text1 = new TextView(this);
                 text1.setText(products.get(i).getName());
-                text1.setLayoutParams(picLp);
+                text1.setLayoutParams(textLp);
                 b1.addView(text1);
 
                 TextView text2 = new TextView(this);
                 text2.setText(products.get(i).getUDate());
-                text2.setLayoutParams(picLp);
+                text2.setLayoutParams(textLp);
                 b1.addView(text2);
 
                 TextView text3 = new TextView(this);
                 text3.setText(products.get(i).getEDate());
-                text3.setLayoutParams(picLp);
+                text3.setLayoutParams(textLp);
                 b1.addView(text3);
 
+                b1.setLayoutParams(prodLp);
                 b1.setBackgroundColor(Color.BLUE);
                 layout.addView(b1);
             } else if (i + 2 == products.size()) {
@@ -96,22 +108,22 @@ public class Home extends AppCompatActivity{
 
                 TextView text1 = new TextView(this);
                 text1.setText(products.get(i).getName());
-                text1.setLayoutParams(picLp);
+                text1.setLayoutParams(textLp);
                 b1.addView(text1);
 
                 TextView text2 = new TextView(this);
                 text2.setText(products.get(i).getUDate());
-                text2.setLayoutParams(picLp);
+                text2.setLayoutParams(textLp);
                 b1.addView(text2);
 
                 TextView text3 = new TextView(this);
                 text3.setText(products.get(i).getEDate());
-                text3.setLayoutParams(picLp);
+                text3.setLayoutParams(textLp);
                 b1.addView(text3);
 
                 b1.setBackgroundColor(Color.BLUE);
                 layoutLp.setMargins(0, 0, 15, 0);
-                b1.setLayoutParams(layoutLp);
+                b1.setLayoutParams(prodLp);
                 layout.addView(b1);
 
                 LinearLayout b2 = new LinearLayout(this);
@@ -125,19 +137,20 @@ public class Home extends AppCompatActivity{
 
                 TextView text21 = new TextView(this);
                 text21.setText(products.get(i + 1).getName());
-                text21.setLayoutParams(picLp);
+                text21.setLayoutParams(textLp);
                 b2.addView(text21);
 
                 TextView text22 = new TextView(this);
                 text22.setText(products.get(i + 1).getUDate());
-                text22.setLayoutParams(picLp);
+                text22.setLayoutParams(textLp);
                 b2.addView(text22);
 
                 TextView text23 = new TextView(this);
                 text23.setText(products.get(i + 1).getEDate());
-                text23.setLayoutParams(picLp);
+                text23.setLayoutParams(textLp);
                 b2.addView(text23);
 
+                b2.setLayoutParams(prodLp);
                 b2.setBackgroundColor(Color.BLUE);
                 layout.addView(b2);
             } else {
@@ -152,22 +165,22 @@ public class Home extends AppCompatActivity{
 
                 TextView text1 = new TextView(this);
                 text1.setText(products.get(i).getName());
-                text1.setLayoutParams(picLp);
+                text1.setLayoutParams(textLp);
                 b1.addView(text1);
 
                 TextView text2 = new TextView(this);
                 text2.setText(products.get(i).getUDate());
-                text2.setLayoutParams(picLp);
+                text2.setLayoutParams(textLp);
                 b1.addView(text2);
 
                 TextView text3 = new TextView(this);
                 text3.setText(products.get(i).getEDate());
-                text3.setLayoutParams(picLp);
+                text3.setLayoutParams(textLp);
                 b1.addView(text3);
 
                 b1.setBackgroundColor(Color.BLUE);
                 layoutLp.setMargins(0, 0, 15, 0);
-                b1.setLayoutParams(layoutLp);
+                b1.setLayoutParams(prodLp);
                 layout.addView(b1);
 
                 LinearLayout b2 = new LinearLayout(this);
@@ -181,22 +194,22 @@ public class Home extends AppCompatActivity{
 
                 TextView text21 = new TextView(this);
                 text21.setText(products.get(i + 1).getName());
-                text21.setLayoutParams(picLp);
+                text21.setLayoutParams(textLp);
                 b2.addView(text21);
 
                 TextView text22 = new TextView(this);
                 text22.setText(products.get(i + 1).getUDate());
-                text22.setLayoutParams(picLp);
+                text22.setLayoutParams(textLp);
                 b2.addView(text22);
 
                 TextView text23 = new TextView(this);
                 text23.setText(products.get(i + 1).getEDate());
-                text23.setLayoutParams(picLp);
+                text23.setLayoutParams(textLp);
                 b2.addView(text23);
 
                 b2.setBackgroundColor(Color.BLUE);
                 layoutLp.setMargins(0, 0, 15, 0);
-                b2.setLayoutParams(layoutLp);
+                b2.setLayoutParams(prodLp);
                 layout.addView(b2);
 
                 LinearLayout b3 = new LinearLayout(this);
@@ -210,19 +223,20 @@ public class Home extends AppCompatActivity{
 
                 TextView text31 = new TextView(this);
                 text31.setText(products.get(i + 1).getName());
-                text31.setLayoutParams(picLp);
+                text31.setLayoutParams(textLp);
                 b3.addView(text31);
 
                 TextView text32 = new TextView(this);
                 text32.setText(products.get(i + 1).getUDate());
-                text32.setLayoutParams(picLp);
+                text32.setLayoutParams(textLp);
                 b3.addView(text32);
 
                 TextView text33 = new TextView(this);
                 text33.setText(products.get(i + 1).getEDate());
-                text33.setLayoutParams(picLp);
+                text33.setLayoutParams(textLp);
                 b3.addView(text33);
 
+                b3.setLayoutParams(prodLp);
                 b3.setBackgroundColor(Color.BLUE);
                 layout.addView(b3);
             }
