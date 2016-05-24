7aac864c73c1add158a9431d7e5a57cae855ce35
Michael Chen <zhenfangchen@Zhenfangs-iMac.attlocal.net>
2016-05-23 17:06:22 -0700

    Hopefully working add

--- app/src/main/java/foodcabinet/foodcabinet/AddProductsScreen.java
+++ app/src/main/java/foodcabinet/foodcabinet/AddProductsScreen.java
@@ -8,44 +8,82 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class AddProductsScreen extends AppCompatActivity {
     private Cabinet cabinet;
-    private Intent intent;
+    private ArrayList<Product> needUpdate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_products_screen);
 
         cabinet = (Cabinet) getIntent().getSerializableExtra("Cabinet");
-    }
+        needUpdate = (ArrayList<Product>) getIntent().getSerializableExtra("NeedUpdates");
 
-    public void takePicture() {
-        Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePicture.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePicture, 1);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.DataEnterLayout);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.setMargins(0, 0, 0, 20);
+        for (int i = 0; i < needUpdate.size(); i++) {
+            TextView name = new TextView(this);
+            name.setText(needUpdate.get(i).getName());
+            name.setLayoutParams(params);
+
+            EditText eDate = new EditText(this);
+            eDate.setHint("Enter days until expired");
+            eDate.setLayoutParams(params);
+            eDate.setId(10000 + i);
+
+            EditText uDate = new EditText(this);
+            uDate.setHint("Enter days until predicted use");
+            uDate.setLayoutParams(params);
+            uDate.setId(20000 + i);
+
+            layout.addView(name);
+            layout.addView(eDate);
+            layout.addView(uDate);
         }
+
+        Button b = new Button(this);
+        b.setHint("Submit Dates");
+        b.setWidth(layout.getWidth());
+        b.setHeight(30);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submitData(v);
+            }
+        });
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            intent = data;
+    public void submitData(View v) {
+        for (int i = 0; i < needUpdate.size(); i++) {
+            needUpdate.get(i).increaseEntered();
+            EditText eDate = (EditText) findViewById(10000+i);
+            Integer eD = Integer.parseInt(eDate.getText().toString());
+            EditText uDate = (EditText) findViewById(20000+i);
+            Integer uD = Integer.parseInt(uDate.getText().toString());
+            needUpdate.get(i).setUDays(uD);
+            needUpdate.get(i).setEDays(eD);
+
+            for (int j = 0; j < cabinet.getCurrentProducts().size(); j++) {
+                if (cabinet.getCurrentProducts().get(j).getName().equals(needUpdate.get(i))) {
+                    cabinet.getCurrentProducts().remove(j);
+                    cabinet.getCurrentProducts().add(needUpdate.get(i));
+                    break;
+                }
+            }
         }
-    }
 
-    public void SubmitData(View v) {
-    	EditText product = (EditText) findViewById(R.id.DataEnterProduct);
-    	EditText date = (EditText) findViewById(R.id.DataEnterDate);
-    	
-    	String prod = product.getText().toString();
-    	Integer days = Integer.parseInt(date.getText().toString());
-    	
-    	Intent submit = new Intent(this, Home.class);
-    	submit.putExtra("ProductName", prod);
-    	submit.putExtra("DateName", days+"");
-    	submit.putExtra("Cabinet", cabinet);
-    	submit.putExtra("Picture", intent);
-    	startActivity(submit);
+        Intent intent = new Intent(this, Home.class);
+        intent.putExtra("Cabinet", cabinet);
+        startActivity(intent);
     }
 
 }
